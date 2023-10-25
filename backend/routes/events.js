2b04b524d35e3bb44c90bf1f8de0b98dcd5d6e44
Ibrahim Semary <92101864+ibrahimsemary@users.noreply.github.com>
2023-10-24 20:15:11 -0500

    added getting set events and recommended events

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -15,7 +15,7 @@ var client = new pg.Client(conString);
 client.connect();
 
 // get all the events associated with userID and event date
-router.get("/get-user-events", async (req, res) => {
+router.get("/get-events", async (req, res) => {
     const user_id = req.query.user_id;
     const event_date = req.query.event_date;
     try {
@@ -27,4 +27,28 @@ router.get("/get-user-events", async (req, res) => {
     }
 });
 
+// get all the non-reccomended events associated with userID and event date
+router.get("/get-set-events", async (req, res) => {
+    const user_id = req.query.user_id;
+    const event_date = req.query.event_date;
+    try {
+        const result = await client.query(`SELECT * FROM events WHERE user_id = ${user_id} AND event_date = '${event_date}' AND task_id is NULL;`);
+        res.send(result.rows);
+    } catch (err) {
+        console.log(err.message);
+        res.send(err.message);
+    }
+});
+// get all the recommended events assciated with user ID and event date
+router.get("/get-recommended-events", async (req, res) => {
+    const user_id = req.query.user_id;
+    const event_date = req.query.event_date;
+    try {
+        const result = await client.query(`SELECT * FROM events WHERE user_id = ${user_id} AND event_date = '${event_date}' AND task_id is NOT NULL;`);
+        res.send(result.rows);
+    } catch (err) {
+        console.log(err.message);
+        res.send(err.message);
+    }
+});
 module.exports = router
