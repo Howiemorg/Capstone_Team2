373c50952a6653e211e942b7c963c9ba4fab2cd3
Ibrahim Semary <92101864+ibrahimsemary@users.noreply.github.com>
2023-10-26 13:07:05 -0500

    connected algo to database

--- backend/algo.js
+++ backend/algo.js
@@ -1,144 +1,199 @@
 function decreaseTimeByOneHour(time) {
-  // Split the time string into hours and minutes
-  const [hours, minutes] = time.split(':').map(Number);
+    // Split the time string into hours and minutes
+    const [hours, minutes, seconds] = time.split(":").map(Number);
 
-  // Calculate the new time
-  let newHours = hours - 1;
-  let newMinutes = minutes;
+    // Calculate the new time
+    let newHours = hours - 1;
 
-  // Check for underflow
-  if (newHours < 0) {
-    newHours = 23; // Wrap around to 23 (11 PM)
-  }
-
-  // Ensure the time components have leading zeros if necessary
-  const formattedHours = String(newHours).padStart(2, '0');
-  const formattedMinutes = String(newMinutes).padStart(2, '0');
-
-  // Return the new time in the 24-hour format
-  return `${formattedHours}:${formattedMinutes}`;
+    // Check for underflow
+    if (newHours < 0) {
+        newHours = 23; // Wrap around to 23 (11 PM)
+    }
+    // Return the new time in the 24-hour format
+    return newHours;
 }
-
-// function listHoursBetween(start, end) {
-//   // Split the start and end times into hours and minutes
-//   const [startHours, startMinutes] = start.split(':').map(Number);
-//   const [endHours, endMinutes] = end.split(':').map(Number);
-
-//   // Initialize an array to store the list of hours
-//   const hoursList = [];
-
-  
-
-//   return hoursList;
-// }
 
 function listHoursBetween(start, end) {
-  // Split the start and end times into hours and minutes
-  const [startHours, startMinutes] = start.split(':').map(Number);
-  const [endHours, endMinutes] = end.split(':');
-  
-  // Initialize an array to store the list of hours
-  const hoursList = [];
-  for(let i = startHours; i <= endHours; i++){
-     let new_hour = i
-     if(new_hour === 24){
-         new_hour = 0
-     }
-     new_hour = String(new_hour).padStart(2, '0');
-     hoursList.push(`${new_hour}:${endMinutes}`)
-  }
-  
-  return hoursList;
-}
+    // Split the start and end times into hours and minutes
+    const [startHours, startMinutes] = start.split(":").map(Number);
+    const [endHours, endMinutes] = end.split(":");
 
-function getScore(a, halfHourDictionary){
-  let s = 0
-  a.forEach(key => {
-    s += halfHourDictionary[key]
-  })
-  return s
-}
-
-
-let halfHourDictionary = {};
-
-for (let hour = 0; hour < 24; hour++) {
-  for (let minute = 0; minute < 60; minute += 60) {
-    const time = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
-    halfHourDictionary[time] = 0;
-  }
-}
-
-let wakeUpTime = '08:00';
-let sleepTime = '24:00';
-
-// let circadianRhythmWork = [1,1,2,5,5,5,5,5,4,4,3,3,2,2,2,3,3,4,4,5,5,5,5,3,3,2,2,1,1];
-let circadianRhythmWork = [1,3,5,5,4,3,2,2,2,3,5,5,5,2,2,1];
-let events = [{
-  start: '09:00',
-  stop: '11:00'
-},
-{
-  start: '14:00',
-  stop: '15:00'
-},
-{
-  start: '19:00',
-  stop: '21:00'
-},
-]
-
-let i = 0;
-
-for (const time in halfHourDictionary) {
-  if (time >= wakeUpTime && time <= sleepTime) {
-    if(i >= circadianRhythmWork.length){
-        break;
+    // Initialize an array to store the list of hours
+    const hoursList = [];
+    for (let i = startHours; i <= endHours; i++) {
+        let new_hour = i;
+        if (new_hour === 24) {
+            new_hour = 0;
+        }
+        new_hour = String(new_hour).padStart(2, "0");
+        hoursList.push(`${new_hour}:${endMinutes}`);
     }
-    halfHourDictionary[time] = circadianRhythmWork[i];
-    i++;
-  }
+
+    return hoursList;
 }
 
-console.log(halfHourDictionary);
+function getScore(a, halfHourDictionary) {
+    let s = 0;
+    a.forEach((key) => {
+        s += halfHourDictionary[key];
+    });
+    return s;
+}
 
+function get_available_intervals(events) {
+    const available_intervals = [];
+    let begin = 0;
+    events.forEach((event) => {
+        if (decreaseTimeByOneHour(event.event_start_time) >= begin) {
+            available_intervals.push({
+                start: begin,
+                end: decreaseTimeByOneHour(event.event_start_time),
+            });
+            begin = parseInt(event.event_end_time.split(":")[0]);
+        } else {
+            begin = parseInt(event.event_end_time.split(":")[0]);
+        }
+    });
+    if (begin != 23) {
+        available_intervals.push({
+            start: begin,
+            end: 23,
+        });
+    }
+    return available_intervals;
+}
+let circadian_rhythm = [
+    0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 5, 5, 4, 3, 2, 2, 2, 3, 5, 5, 5, 2, 2, 1,
+];
 
-// go through and get all the free intervals in which it is empty
-const avaiable_intervals = []
-let begin = "00:00";
-events.forEach(event => {
-  avaiable_intervals.push(
+let events = [
     {
-      start: begin,
-      end: decreaseTimeByOneHour(event.start)
-    }
-  )
-  begin = event.stop
+        event_block_id: 13,
+        event_name: "BIO331",
+        event_start_time: "09:00:00",
+        event_end_time: "10:00:00",
+        reminder_id: null,
+        user_id: 1,
+        task_id: null,
+        work_done_pct: null,
+        repetition_duration: {
+            minutes: 50,
+        },
+        repetition_days: [1, 3, 5],
+        repetition_frequency: "Weekly",
+        event_date: "2023-10-31T05:00:00.000Z",
+    },
+    {
+        event_block_id: 11,
+        event_name: "ART335",
+        event_start_time: "10:00:00",
+        event_end_time: "12:00:00",
+        reminder_id: null,
+        user_id: 1,
+        task_id: null,
+        work_done_pct: null,
+        repetition_duration: {
+            hours: 1,
+            minutes: 30,
+        },
+        repetition_days: [1, 3, 5],
+        repetition_frequency: "Weekly",
+        event_date: "2023-10-31T05:00:00.000Z",
+    },
+    {
+        event_block_id: 14,
+        event_name: "Study Colory Theory",
+        event_start_time: "12:00:00",
+        event_end_time: "13:00:00",
+        reminder_id: null,
+        user_id: 1,
+        task_id: 7,
+        work_done_pct: null,
+        repetition_duration: {
+            minutes: 30,
+        },
+        repetition_days: [1, 3, 5],
+        repetition_frequency: "Weekly",
+        event_date: "2023-10-31T05:00:00.000Z",
+    },
+    {
+        event_block_id: 12,
+        event_name: "ART482",
+        event_start_time: "14:00:00",
+        event_end_time: "15:00:00",
+        reminder_id: null,
+        user_id: 1,
+        task_id: null,
+        work_done_pct: null,
+        repetition_duration: {
+            hours: 1,
+        },
+        repetition_days: [2, 4],
+        repetition_frequency: "Weekly",
+        event_date: "2023-10-31T05:00:00.000Z",
+    },
+];
+
+const tasks = [
+  {
+      user_id: 1,
+      task_id: 5,
+      task_name: "Work on Masterpiece",
+      task_start_date: "2023-10-31T05:00:00.000Z",
+      task_due_date: "2023-11-02T05:00:00.000Z",
+      progress_percent: 0,
+      priority_level: 3,
+      estimate_completion_time: 60,
+      completion_date: null,
+  },
+  {
+      user_id: 1,
+      task_id: 9,
+      task_name: "Studying",
+      task_start_date: "2023-10-31T05:00:00.000Z",
+      task_due_date: "2023-11-02T05:00:00.000Z",
+      progress_percent: 0,
+      priority_level: null,
+      estimate_completion_time: 120,
+      completion_date: null,
+  },
+  {
+      user_id: 1,
+      task_id: 8,
+      task_name: "Studying",
+      task_start_date: "2023-10-31T05:00:00.000Z",
+      task_due_date: "2023-11-02T05:00:00.000Z",
+      progress_percent: 0,
+      priority_level: null,
+      estimate_completion_time: 60,
+      completion_date: null,
+  },
+];
+
+let available_intervals = get_available_intervals(events);
+
+tasks.forEach(task => {
+  const width = (task.estimate_completion_time/60)-1
+  console.log(width)
+  let best_intervals
 })
-if(begin != "23:00"){
-  avaiable_intervals.push({
-    start:begin,
-    end: "23:00"
-  })
-}
+
+// let task_time = 2
+// //Find the best time interval for each available time
+// let best_intervals = []
+// avaiable_intervals.forEach(interval => {
+//     let keys = listHoursBetween(interval.start, interval.end);
+//     // console.log(keys)
+//     r = task_time-1
+//     for(let l = 0; r < keys.length; l++){
+//       console.log(keys[l], keys[r], getScore(keys.slice(l,r+1), halfHourDictionary))
+//       best_intervals.push([getScore(keys.slice(l,r+1), halfHourDictionary), [keys[l], keys[r]]]);
+//       r++
+//     }
+//     console.log("")
+// })
+// best_intervals.sort((a,b) => b[0] - a[0])
 
 
-console.log(avaiable_intervals)
-
-let task_time = 2
-//Find the best time interval for each available time
-let best_intervals = []
-avaiable_intervals.forEach(interval => {
-    let keys = listHoursBetween(interval.start, interval.end);
-    // console.log(keys)
-    r = task_time-1
-    for(let l = 0; r < keys.length; l++){
-      console.log(keys[l], keys[r], getScore(keys.slice(l,r+1), halfHourDictionary))
-      best_intervals.push([getScore(keys.slice(l,r+1), halfHourDictionary), [keys[l], keys[r]]]);
-      r++
-    }
-    console.log("")
-})
-best_intervals.sort((a,b) => b[0] - a[0])
-console.log(best_intervals)
-
+console.log();
+module.exports = get_available_intervals
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,7 +1,9 @@
-const dotenv = require('dotenv').config();
-const express = require('express')
-const pg = require('pg');
-const router = express.Router()
+const get_available_intervals = require('../algo');
+
+const dotenv = require("dotenv").config();
+const express = require("express");
+const pg = require("pg");
+const router = express.Router();
 
 const conString = {
     host: process.env.DB_HOST,
@@ -9,11 +11,10 @@ const conString = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    ssl: process.env.DB_SSL === 'true',
+    ssl: process.env.DB_SSL === "true",
 };
 var client = new pg.Client(conString);
 client.connect();
-
 
 // get all uncompleted events for a user
 router.get("/get-uncompleted-tasks", async (req, res) => {
@@ -33,4 +34,35 @@ router.get("/get-uncompleted-tasks", async (req, res) => {
     }
 });
 
-module.exports = router
+router.post("/get-recommendations", async (req, res) => {
+    const user_id = req.query.user_id;
+    const selected_date = req.query.selected_date;
+    try {
+        // get all the tasks from DB
+        const query = {
+            text: "SELECT * FROM tasks WHERE user_id = $1 AND completion_date IS NULL AND task_start_date = $2",
+            values: [user_id, selected_date],
+        };
+        let tasks = await client.query(query);
+        tasks = tasks.rows;
+        // get all the events from the DB
+        let events = await client.query(
+            `SELECT * FROM events WHERE user_id = ${user_id} AND event_date = '${selected_date}' ORDER BY event_start_time;`
+        );
+        events = events.rows;
+        //get the circadian rhythm array
+        let circadian_rhythm = await client.query(
+            `SELECT circadian_rhythm from users WHERE user_id = ${user_id};`
+        );
+        circadian_rhythm = circadian_rhythm.rows[0]["circadian_rhythm"];
+
+        //get empty intervals
+        res.send(get_available_intervals(events));
+
+        //success
+    } catch (err) {
+        console.log(err.message);
+        res.send(err.message);
+    }
+});
+module.exports = router;
