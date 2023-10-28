8910cbbb24b567e50b12c15e78da27a12f83fa8e
Howiemorg <morgenthalerhowie@yahoo.com>
2023-10-28 10:58:21 -0500

    Algorithm reschedules

--- backend/algo.js
+++ backend/algo.js
@@ -1,3 +1,7 @@
+let circadian_rhythm = [
+  0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 5, 5, 4, 3, 2, 2, 2, 4, 5, 5, 5, 2, 2, 1,
+];
+
 function decreaseTimeByOneHour(time) {
   // Split the time string into hours and minutes
   const [hours, minutes, seconds] = time.split(":").map(Number);
@@ -15,13 +19,12 @@ function decreaseTimeByOneHour(time) {
 
 function getScore(a) {
   let s = 0;
-  for(let num of a){
-    if(num === 0){
-        console.log("NUM IS " + num)
-        return 0;
+  for (let num of a) {
+    if (num === 0) {
+      return 0;
     }
     s += num;
-  };
+  }
   return s;
 }
 function isConflicting(number, lowerBound, upperBound) {
@@ -51,18 +54,18 @@ function get_available_intervals(events) {
   return available_intervals;
 }
 
-function get_best_time_intervals(available_interval, curr_task) {
+function get_best_time_intervals(available_intervals, curr_task) {
   let task_time = curr_task.estimate_completion_time / 60;
   let best_intervals = [];
 
   available_intervals.forEach((interval) => {
     let r = interval.start + task_time - 1;
     for (let l = interval.start; r <= interval.end; l++) {
-        const score = getScore(circadian_rhythm.slice(l, r + 1));
-        if(!score){
-            r++;
-            continue;
-        }
+      const score = getScore(circadian_rhythm.slice(l, r + 1));
+      if (!score) {
+        r++;
+        continue;
+      }
       best_intervals.push([score, l, r]);
       r++;
     }
@@ -70,10 +73,86 @@ function get_best_time_intervals(available_interval, curr_task) {
   best_intervals.sort((a, b) => b[0] - a[0]);
   return best_intervals;
 }
+function algorithm(events, tasks, reschedule_value) {
+  tasks.sort((a, b) => {
+    if (a.priority_level == b.priority_level) {
+      return b.estimate_completion_time - a.estimate_completion_time;
+    }
+    return b.priority_level - a.priority_level;
+  });
 
-let circadian_rhythm = [
-  0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 5, 5, 4, 3, 2, 2, 2, 4, 5, 5, 5, 2, 2, 1,
-];
+  let available_intervals = get_available_intervals(events);
+  console.log(available_intervals);
+
+  const already_recommended = [];
+  const new_events = [];
+  tasks.forEach((task) => {
+    let curr_task_recommendations = get_best_time_intervals(
+      available_intervals,
+      task
+    );
+
+    let best_available_times = [];
+    //pick first one that is not already reccommended
+    for (const rec of curr_task_recommendations) {
+      let conflicting = false;
+      for (const already_recommended_t of already_recommended) {
+        if (
+          isConflicting(
+            rec[1],
+            already_recommended_t.start,
+            already_recommended_t.end
+          ) ||
+          isConflicting(
+            rec[2],
+            already_recommended_t.start,
+            already_recommended_t.end
+          )
+        ) {
+          conflicting = true;
+          break;
+        }
+      }
+      if (!conflicting) {
+        best_available_times.push({
+          score: rec[0],
+          start: rec[1],
+          end: rec[2],
+        });
+      }
+    }
+    const best_time =
+      best_available_times[reschedule_value % best_available_times.length];
+    console.log(best_available_times.length)
+    new_events.push({
+      name: task.task_name,
+      score: best_time.score,
+      time: `${best_time.start}:00 - ${best_time.end + 1}:00`,
+    });
+    already_recommended.push({ start: best_time.start, end: best_time.end });
+  });
+
+  console.log("Done");
+  console.log(new_events);
+
+  // let task_time = 2
+  // //Find the best time interval for each available time
+  // let best_intervals = []
+  // avaiable_intervals.forEach(interval => {
+  //     let keys = listHoursBetween(interval.start, interval.end);
+  //     // console.log(keys)
+  //     r = task_time-1
+  //     for(let l = 0; r < keys.length; l++){
+  //       console.log(keys[l], keys[r], getScore(keys.slice(l,r+1), halfHourDictionary))
+  //       best_intervals.push([getScore(keys.slice(l,r+1), halfHourDictionary), [keys[l], keys[r]]]);
+  //       r++
+  //     }
+  //     console.log("")
+  // })
+  // best_intervals.sort((a,b) => b[0] - a[0])
+
+  console.log();
+}
 
 let events = [
   {
@@ -92,80 +171,80 @@ let events = [
     repetition_frequency: "Weekly",
     event_date: "2023-10-31T05:00:00.000Z",
   },
-//   {
-//     event_block_id: 11,
-//     event_name: "ART335",
-//     event_start_time: "10:00:00",
-//     event_end_time: "12:00:00",
-//     reminder_id: null,
-//     user_id: 1,
-//     task_id: null,
-//     work_done_pct: null,
-//     repetition_duration: {
-//       hours: 1,
-//       minutes: 30,
-//     },
-//     repetition_days: [1, 3, 5],
-//     repetition_frequency: "Weekly",
-//     event_date: "2023-10-31T05:00:00.000Z",
-//   },
-//   {
-//     event_block_id: 14,
-//     event_name: "Study Colory Theory",
-//     event_start_time: "12:00:00",
-//     event_end_time: "13:00:00",
-//     reminder_id: null,
-//     user_id: 1,
-//     task_id: 7,
-//     work_done_pct: null,
-//     repetition_duration: {
-//       minutes: 30,
-//     },
-//     repetition_days: [1, 3, 5],
-//     repetition_frequency: "Weekly",
-//     event_date: "2023-10-31T05:00:00.000Z",
-//   },
-//   {
-//     event_block_id: 12,
-//     event_name: "ART482",
-//     event_start_time: "14:00:00",
-//     event_end_time: "15:00:00",
-//     reminder_id: null,
-//     user_id: 1,
-//     task_id: null,
-//     work_done_pct: null,
-//     repetition_duration: {
-//       hours: 1,
-//     },
-//     repetition_days: [2, 4],
-//     repetition_frequency: "Weekly",
-//     event_date: "2023-10-31T05:00:00.000Z",
-//   },
+  {
+    event_block_id: 11,
+    event_name: "ART335",
+    event_start_time: "10:00:00",
+    event_end_time: "12:00:00",
+    reminder_id: null,
+    user_id: 1,
+    task_id: null,
+    work_done_pct: null,
+    repetition_duration: {
+      hours: 1,
+      minutes: 30,
+    },
+    repetition_days: [1, 3, 5],
+    repetition_frequency: "Weekly",
+    event_date: "2023-10-31T05:00:00.000Z",
+  },
+  {
+    event_block_id: 14,
+    event_name: "Study Colory Theory",
+    event_start_time: "12:00:00",
+    event_end_time: "13:00:00",
+    reminder_id: null,
+    user_id: 1,
+    task_id: 7,
+    work_done_pct: null,
+    repetition_duration: {
+      minutes: 30,
+    },
+    repetition_days: [1, 3, 5],
+    repetition_frequency: "Weekly",
+    event_date: "2023-10-31T05:00:00.000Z",
+  },
+  {
+    event_block_id: 12,
+    event_name: "ART482",
+    event_start_time: "14:00:00",
+    event_end_time: "15:00:00",
+    reminder_id: null,
+    user_id: 1,
+    task_id: null,
+    work_done_pct: null,
+    repetition_duration: {
+      hours: 1,
+    },
+    repetition_days: [2, 4],
+    repetition_frequency: "Weekly",
+    event_date: "2023-10-31T05:00:00.000Z",
+  },
 ];
 
 const tasks = [
-  {
-    user_id: 1,
-    task_id: 5,
-    task_name: "Work on Masterpiece",
-    task_start_date: "2023-10-31T05:00:00.000Z",
-    task_due_date: "2023-11-02T05:00:00.000Z",
-    progress_percent: 0,
-    priority_level: 4,
-    estimate_completion_time: 60,
-    completion_date: null,
-  },
-  {
-    user_id: 1,
-    task_id: 9,
-    task_name: "Studying",
-    task_start_date: "2023-10-31T05:00:00.000Z",
-    task_due_date: "2023-11-02T05:00:00.000Z",
-    progress_percent: 0,
-    priority_level: 2,
-    estimate_completion_time: 120,
-    completion_date: null,
-  },
+  //   {
+  //     user_id: 1,
+  //     task_id: 5,
+  //     task_name: "Work on Masterpiece",
+  //     task_start_date: "2023-10-31T05:00:00.000Z",
+  //     task_due_date: "2023-11-02T05:00:00.000Z",
+  //     progress_percent: 0,
+  //     priority_level: 4,
+  //     estimate_completion_time: 60,
+  //     completion_date: null,
+  //   },
+  //   {
+  //     user_id: 1,
+  //     task_id: 9,
+  //     task_name: "Studying",
+  //     task_start_date: "2023-10-31T05:00:00.000Z",
+  //     task_due_date: "2023-11-02T05:00:00.000Z",
+  //     progress_percent: 0,
+  //     priority_level: 2,
+  //     estimate_completion_time: 120,
+  //     completion_date: null,
+  //   },
   {
     user_id: 1,
     task_id: 8,
@@ -179,73 +258,6 @@ const tasks = [
   },
 ];
 
-tasks.sort((a, b) => {
-  if (a.priority_level == b.priority_level) {
-    return b.estimate_completion_time - a.estimate_completion_time;
-  }
-  return b.priority_level - a.priority_level;
-});
+algorithm(events, tasks, 12);
 
-let available_intervals = get_available_intervals(events);
-console.log(available_intervals);
-
-const already_recommended = [];
-const new_events = [];
-tasks.forEach((task) => {
-  let curr_task_recommendations = get_best_time_intervals(
-    available_intervals,
-    task
-  );
-
-  //pick first one that is not already reccommended
-  for (const rec of curr_task_recommendations) {
-    let conflicting = false;
-    for (const already_recommended_t of already_recommended) {
-      if (
-        isConflicting(
-          rec[1],
-          already_recommended_t.start,
-          already_recommended_t.end
-        ) ||
-        isConflicting(
-          rec[2],
-          already_recommended_t.start,
-          already_recommended_t.end
-        )
-      ) {
-        conflicting = true;
-        break;
-      }
-    }
-    if (!conflicting) {
-      new_events.push({
-        name: task.task_name,
-        score: rec[0],
-        time: `${rec[1]}:00 - ${rec[2] + 1}:00`,
-      });
-      already_recommended.push({ start: rec[1], end: rec[2] });
-      break;
-    }
-  }
-});
-console.log("Done")
-console.log(new_events);
-
-// let task_time = 2
-// //Find the best time interval for each available time
-// let best_intervals = []
-// avaiable_intervals.forEach(interval => {
-//     let keys = listHoursBetween(interval.start, interval.end);
-//     // console.log(keys)
-//     r = task_time-1
-//     for(let l = 0; r < keys.length; l++){
-//       console.log(keys[l], keys[r], getScore(keys.slice(l,r+1), halfHourDictionary))
-//       best_intervals.push([getScore(keys.slice(l,r+1), halfHourDictionary), [keys[l], keys[r]]]);
-//       r++
-//     }
-//     console.log("")
-// })
-// best_intervals.sort((a,b) => b[0] - a[0])
-
-console.log();
 module.exports = get_available_intervals;
