cc48c8b47bf8f86409158d0c3d94dc999421e593
LuisM-M <2002luismm@gmail.com>
2023-11-04 11:58:51 -0500

    replacing the vercel backend hosting with my own

--- frontend/src/api/vercel.js
+++ frontend/src/api/vercel.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://capstone-backend-charles-tran.vercel.app/"
+    baseURL: "https://project-backend-nwatoqdm6-luis-projects-0b14a070.vercel.app/"
 })
