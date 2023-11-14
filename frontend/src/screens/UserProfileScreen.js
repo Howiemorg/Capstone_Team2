911180892ef338c96dfd06a52ca63d43a8a945f0
Adidev Mohapatra <adidev@tamu.edu>
2023-11-14 15:49:03 -0600

    UserProfile.js set up connections

---
+++ frontend/src/screens/UserProfileScreen.js
@@ -0,0 +1,4 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import { useSelector } from 'react-redux'; 
+import vercelClient from './path/to/vercel';
