410b335b41775cd71adb59233e938658a4b6a3d7
Adidev Mohapatra <adidev@tamu.edu>
2023-11-27 15:47:41 -0600

    Adding userscreen route

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,7 @@ import HomeScreen from "./src/screens/HomeScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import TaskScreen from "./src/screens/TaskScreen"
+import UserProfileScreen from "./src/screens/UserProfileScreen";
 import { createAppContainer } from "react-navigation";
 import CustomProvider from "./src/store/provider";
 
@@ -12,6 +13,7 @@ const Stack = createStackNavigator(
     SignUp: SignUpScreen,
     Login: LoginScreen,
     Task: TaskScreen,
+    UserProfile : UserProfileScreen
   },
   {
     initialRouteName: "Login",
